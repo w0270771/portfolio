b21db83bf809a40bb0b9b414ed3078817b508f21
Nayeema Lail <w0270771@nscc.ca>
2016-05-04 05:19:35 -0700

    init

---
+++ app/controllers/clean_blogs_controller.rb
@@ -0,0 +1,18 @@
+class CleanBlogsController < ApplicationController
+	layout "clean_blog"
+
+	def index
+	end
+
+	def about
+	end
+
+	def portfolio
+	end
+
+	def project
+	end
+
+	def contact
+	end
+end
